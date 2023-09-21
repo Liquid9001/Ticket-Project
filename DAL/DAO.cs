a349bf9494a0706ccb6deac49c8b08e143c915b4
enesb <enesb@ECB>
2023-09-21 10:08:15 +0200

    Add project files.

---
+++ DAL/DAO.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using Model;
+using MongoDB.Bson.Serialization;
+
+namespace DAL
+{
+    public class DAO
+    {
+        private MongoClient client;
+
+        public DAO()
+        {
+            client = new MongoClient("mongodb+srv://liquid:8XhofoQHbeuqnI18@cluster0.odpygx7.mongodb.net/?retryWrites=true&w=majority");
+        }                           //mongodb+srv://liquid:8XhofoQHbeuqnI18@cluster0.odpygx7.mongodb.net/
+        //mongodb+srv://liquid:8XhofoQHbeuqnI18@cluster0.odpygx7.mongodb.net/
+
+        public List<Databases_Model> GetDatabases()
+        {
+            List<Databases_Model> all_databases = new List<Databases_Model>();
+            
+            foreach (BsonDocument db in client.ListDatabases().ToList())
+            {
+                all_databases.Add(BsonSerializer.Deserialize<Databases_Model>(db));
+            }
+            return all_databases;
+        }
+    }
+
+
+}
