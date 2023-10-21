5d52066c4e412284766c92aa75be7d06a6effaae
Liquid9001 <699275@student.inholland.nl>
2023-10-21 16:48:15 +0200

    added an employee logic to logic layer with some code

---
+++ MongoProject/Logic/EmployeeLogic.cs
@@ -0,0 +1,40 @@
+﻿using MongoProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoProject.Logic
+{
+    public class EmployeeLogic
+    {
+      /*  public Employee ValidateUser(Employee employee)
+        {
+            
+            try
+            {
+                Employee staffvalidate = _staffDAO.GetStaffByUsername(em.username);
+                if (staffvalidate.password == HashPassword(employee.password))
+                {
+                    return staffvalidate;
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }*/
+
+        string HashPassword(string passcode)
+        {
+            SHA256 sha256 = SHA256.Create();
+            var passwordInBytes = Encoding.Default.GetBytes(passcode);
+            var hassedPassword = sha256.ComputeHash(passwordInBytes);
+            return Convert.ToHexString(hassedPassword);
+        }
+    }
+}
